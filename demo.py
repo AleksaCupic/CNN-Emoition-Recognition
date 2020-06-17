71e47175e0d6e7ea8fb969f0bdf7b31c3f218b82
AleksaCupic <47996071+AleksaCupic@users.noreply.github.com>
2020-06-17 10:11:45 -0400

    Cleaned up python code

--- demo.py
+++ demo.py
@@ -2,60 +2,55 @@ import numpy as np
 import cv2
 import matplotlib.image as mpimg
 import os
-
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras import models
 
 def cropImage():
-    image = cv2.imread('{folder_path}/{image.jpg}')
+    image = cv2.imread('Folder1/image.jpg')
+    print("MY IMAGE")
+    print(image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     faces = faceCascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3, minSize=(30, 30))
-    
+
     print("[INFO] Found {0} Faces.".format(len(faces)))
-    
+
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         roi_color = image[y:y + h, x:x + w]
         print("[INFO] Object found. Saving locally.")
-        cv2.imwrite('{folder_for_detected_faces}/{image.jpg}', roi_color)
+        cv2.imwrite('detectedFaces/image.jpg', roi_color)
+        cv2.imwrite('detectedFaces/images/image.jpg', roi_color)
 
 
+def predictImage():
 
-#May need to be deleted
-
-def augumentImage():
-    from tensorflow.keras.preprocessing.image import ImageDataGenerator
-    
-    my_test_datagen  = ImageDataGenerator( rescale = 1.0/255, )
-        
-    my_test_generator=my_test_datagen.flow_from_directory('{folder_where_you_stored_detected_face}',
-                                                        class_mode  = 'categorical',
-                                                        #color_mode='grayscale',
-                                                        target_size = (64, 64),
-                                                        batch_size=128)
-    return my_test_generator
-
-def displayImage():
-    import matplotlib.pyplot as plt
-
-    from tensorflow.keras import models
-    my_test_generator=augumentImage()
+    print("Loading model....")
     model_kaggle = models.load_model('clean_kaggle.h5')
-    y_img_batch, y_class_batch = my_test_generator[0]
-    y_pred = np.argmax(model_kaggle.predict(y_img_batch),-1)
-    
-    
-    x_batch, y_batch = my_test_generator[0]
-    
-    emotions=['Angry','Happy','Sad','Surprised']
-    
-    for i in range (0,1):
-        plt.title(emotions[y_pred[i]])
-        #image = x_batch[i]
-        img = mpimg.imread('{folder_with_detected_face}/{image.jpg}')
-        plt.imshow(img)
-        plt.show()
 
+
+    # SCALE IMAGE AND PREDICT
+
+    imasd=image.load_img('detectedFaces/image.jpg', target_size=(64,64))
+    imgArr=image.img_to_array(imasd)/255.
+    image1 = np.expand_dims(imgArr, axis=0)
+    result=model_kaggle.predict(image1)
+    print("Prediction: ")
+    print(result)
+    maxPred=np.argmax(result,-1)
+    print(maxPred)
+
+
+    emotions=['Angry','Happy','Sad','Surprised']
+
+
+    plt.title(emotions[maxPred[0]])
+    #image = x_batch[i]
+    img = mpimg.imread('detectedFaces/image.jpg')
+    plt.imshow(img)
+    plt.show();
 
 
 
@@ -68,13 +63,12 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 420)
 while cap.isOpened():
     flags, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Emotion Classifier',gray)
+    cv2.imshow('Emotion Classifier', gray)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
-        cv2.imwrite('{folder_path}/{image.jpg}',frame)
+        cv2.imwrite('Folder1/image.jpg',frame)
         cap.release()
         cv2.destroyAllWindows()
         cropImage()
-        displayImage()
+        predictImage()
         break
-
